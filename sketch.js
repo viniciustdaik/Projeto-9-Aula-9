66fbcc0c9ced2c27c2d868e91b548b62f0e00a94
viniciustdaik <96190816+viniciustdaik@users.noreply.github.com>
2021-12-31 12:08:56 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,32 +12,36 @@ function setup() {
 function draw() {
   background('orange');
 
-  if(keyIsDown(RIGHT_ARROW)) {
+  if(keyIsDown(RIGHT_ARROW)||keyDown("D")) {
     box.position.x = box.position.x +5;
     background('green');
   }
-  if(keyIsDown(LEFT_ARROW)){
+  if(keyIsDown(LEFT_ARROW)||keyDown("A")){
     box.position.x = box.position.x -5;
     background('blue');
   }
-  if(keyIsDown(UP_ARROW)){
+  if(keyIsDown(UP_ARROW)||keyDown("W")){
     box.position.y = box.position.y -5;
     background('pink');
   }
-  if(keyIsDown(DOWN_ARROW)){
+  if(keyIsDown(DOWN_ARROW)||keyDown("S")){
     box.position.y = box.position.y +5;
     background('red');
   }
-  if(keyIsDown(DOWN_ARROW)&&keyIsDown(LEFT_ARROW)){
+  if(keyIsDown(DOWN_ARROW)&&keyIsDown(LEFT_ARROW)||
+  keyDown("S")&&keyDown("A")){
     background('yellow');
   }
-  if(keyIsDown(DOWN_ARROW)&&keyIsDown(RIGHT_ARROW)){
+  if(keyIsDown(DOWN_ARROW)&&keyIsDown(RIGHT_ARROW)||
+  keyDown("S")&&keyDown("D")){
     background('brown');
   }
-  if(keyIsDown(UP_ARROW)&&keyIsDown(LEFT_ARROW)){
+  if(keyIsDown(UP_ARROW)&&keyIsDown(LEFT_ARROW)||
+  keyDown("W")&&keyDown("A")){
     background('purple');
   }
-  if(keyIsDown(UP_ARROW)&&keyIsDown(RIGHT_ARROW)){
+  if(keyIsDown(UP_ARROW)&&keyIsDown(RIGHT_ARROW)||
+  keyDown("W")&&keyDown("D")){
     background('brown');
   }
   box.collide(edges[0]);
